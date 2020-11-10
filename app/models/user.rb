55a2fb34149526c74f7b2b2878b62d00804fea30
okadahideya <73088476+okadahideya@users.noreply.github.com>
2020-11-10 14:12:31 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :birthday, presence: true
-  
-  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6}+\z/i}
-
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角(漢字）文字を使用してください' } do
-    validates :first_name
-    validates :second_name
-  end
-  with_options presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナ文字を使用してください' } do
-    validates :first_name_k
-    validates :second_name_k
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6}+\z/i}
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角(漢字）文字を使用してください' }
+    validates :second_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角(漢字）文字を使用してください' }
+    validates :first_name_k, format: {with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナ文字を使用してください' }
+    validates :second_name_k, format: {with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナ文字を使用してください' }
   end
 end
