0c4e713a89f5e5d2863fc7bcbc19a8cc12de5f1f
okadahideya <73088476+okadahideya@users.noreply.github.com>
2020-11-10 16:36:21 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,4 +8,10 @@ class ItemsController < ApplicationController
 
   def cerate
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image).merge(user_id: current_user.id)
+  end
 end
