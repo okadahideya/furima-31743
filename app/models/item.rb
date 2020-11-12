4b3c75bb4f51e0740bff48679d08df32b2c0ec12
okadahideya <73088476+okadahideya@users.noreply.github.com>
2020-11-12 13:39:37 +0900

    商品一覧表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,11 +3,11 @@ class Item < ApplicationRecord
   belongs_to :user
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category
-  belongs_to_active_hash :status
-  belongs_to_active_hash :burden
-  belongs_to_active_hash :prefecture
-  belongs_to_active_hash :day
+  belongs_to_active_hash :category_genre
+  belongs_to_active_hash :status_genre
+  belongs_to_active_hash :delivery_burden_genre
+  belongs_to_active_hash :prefecture_genre
+  belongs_to_active_hash :delivery_days_genre
 
   with_options presence: true do
     validates :image
