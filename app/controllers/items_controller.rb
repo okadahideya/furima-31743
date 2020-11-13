9540449cb72501f8bc5b64d9edaa717496180b26
okadahideya <73088476+okadahideya@users.noreply.github.com>
2020-11-13 15:16:23 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,6 @@
 class ItemsController < ApplicationController
-<<<<<<< HEAD
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :move_to_index, only: [:edit]
-=======
-  before_action :set_item, only: [:edit]
-  before_action :move_to_index, except: [:index, :show]
->>>>>>> 408da993d17361adfa033fa63c47a87a1a7a5ae8
+  before_action :move_to_index, only: [:edit, :destroy]
 
   def index
     @item = Item.all.order(created_at: :desc)
@@ -56,23 +51,14 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :name, :explanation, :category_genre_id, :status_genre_id, :delivery_burden_genre_id, :prefecture_genre_id, :delivery_days_genre_id, :price).merge(user_id: current_user.id)
   end
 
-<<<<<<< HEAD
   def set_item
     @item = Item.find(params[:id])
   end
 
-=======
-
-
-
-  def set_item
-    @item  = Item.find(params[:id])
-  end
-
-
->>>>>>> 408da993d17361adfa033fa63c47a87a1a7a5ae8
   def move_to_index
     @item = Item.find(params[:id])
-    redirect_to action: :index unless user_signed_in? && current_user.id == @item.user.id
+    unless user_signed_in? && current_user.id == @item.user.id
+    redirect_to action: :index 
+    end
   end
 end
