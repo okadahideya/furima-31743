49d8f50a63650c21e4737008a35af1ac46d61e60
okadahideya <73088476+okadahideya@users.noreply.github.com>
2020-11-15 12:23:49 +0900

    バリデーション

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,3 +1,13 @@
 class Delivery < ApplicationRecord
   belongs_to :order
+  
+  validates :build_name
+  with_options presence: true do
+    validates  :postal_code
+    validates  :prefecture_genre_id
+    validates  :municipality
+    validates  :address
+    validates  :phone_number 
+    validates  :order
+  end
 end
