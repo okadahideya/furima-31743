5d84091f22d4aeeddf0a61af26ca7b067adac4cd
okadahideya <73088476+okadahideya@users.noreply.github.com>
2020-11-18 13:35:29 +0900

    修正

--- spec/models/order_delivery_spec.rb
+++ spec/models/order_delivery_spec.rb
@@ -1,15 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe OrderDelivery, type: :model do
-  describe '#create' do
     before do
       @order = FactoryBot.build(:order_delivery)
     end
 
-    it '全ての記載があれば購入できる' do
-      expect(@order).to be_valid
+    describe '#create' do
+    context '購入ができるとき'do
+      it '全ての記載があれば購入できる' do
+        expect(@order).to be_valid
+      end
+
+      it 'build_nameが空白でも登録できる' do
+        @order.build_name = nil
+        expect(@order).to be_valid
+      end
     end
 
+    context'購入ができないとき' do
     it 'postal_codeが空白だと購入できない' do
       @order.postal_code = nil
       @order.valid?
@@ -20,6 +28,12 @@ RSpec.describe OrderDelivery, type: :model do
       @order.prefecture_genre_id = nil
       @order.valid?
       expect(@order.errors.full_messages).to include("Prefecture genre can't be blank", 'Prefecture genre is not a number')
+    end
+
+    it 'prefecture_genre_idが1以外でないと購入できないこと' do
+      @order.prefecture_genre_id = 1
+      @order.valid?
+      expect(@order.errors.full_messages).to include("Prefecture genre must be other than 1")
     end
 
     it 'municipalityが空白だと購入できない' do
@@ -47,15 +61,16 @@ RSpec.describe OrderDelivery, type: :model do
     end
 
     it 'postal_codeはハイフンがないと購入できない' do
-      @order.postal_code = 1_234_567
+      @order.postal_code = 1234567
       @order.valid?
       expect(@order.errors.full_messages).to include('Postal code is invalid')
     end
 
     it 'phone_numberが11桁以内だと購入できない' do
-      @order.phone_number = 123_456_789_123
+      @order.phone_number = 123456789123
       @order.valid?
       expect(@order.errors.full_messages).to include('Phone number is invalid')
     end
+  end
   end
 end
