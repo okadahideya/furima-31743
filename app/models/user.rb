f4c4866d4f81cc6ba630c29555101756b831ee26
okadahideya <73088476+okadahideya@users.noreply.github.com>
2020-11-18 17:20:28 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,8 @@ class User < ApplicationRecord
     validates :nickname
     validates :birthday
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角(漢字）文字を使用してください' }
-    validates :second_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角(漢字）文字を使用してください' }
+    validates :first_name, format: { with: /\A[一-龥]+\z/, message: '全角(漢字）文字を使用してください' }
+    validates :second_name, format: { with: /\A[一-龥]+\z/, message: '全角(漢字）文字を使用してください' }
     validates :first_name_k, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナ文字を使用してください' }
     validates :second_name_k, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナ文字を使用してください' }
   end
