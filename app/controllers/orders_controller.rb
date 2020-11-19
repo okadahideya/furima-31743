e8de1c76e99d11bfe5ca8e260e7ae17c40fc00c0
okadahideya <73088476+okadahideya@users.noreply.github.com>
2020-11-19 12:03:36 +0900

    修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -25,7 +25,7 @@ class OrdersController < ApplicationController
   end
 
   def move_to_index
-    redirect_to root_path if current_user.id && @item.order.present?
+    redirect_to root_path if current_user.id == @item.user_id || @item.order.present? 
   end
 
   def pay_item
